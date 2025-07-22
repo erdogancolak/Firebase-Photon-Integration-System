8839741430ac4ee85a2284d0c84c1581f142c245
erd.colak <erd.colak0@gmail.com>
2025-07-22 17:33:13 +0300

    FirebaseAuth
    FirebaseAuth Connect Test and UI

---
+++ Assets/Scripts/Managers/FirebaseInitManager.cs
@@ -0,0 +1,29 @@
+﻿using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseInitManager : MonoBehaviour
+{
+    [SerializeField] private Image firebaseAuthControllerImage;
+    [SerializeField] private Color connectedColor, errorColor;
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var status = task.Result;
+            if (status == DependencyStatus.Available)
+            {
+                Debug.Log("<color=green>===========[FIREBASE CONNECTED ✅]===========</color>");
+                firebaseAuthControllerImage.color = connectedColor;
+
+                FirebaseApp app = FirebaseApp.DefaultInstance;
+            }
+            else
+            {
+                Debug.Log("<color=green>===========[FIREBASE FAILED ❌]===========</color>");
+                firebaseAuthControllerImage.color=errorColor;
+            }
+        });
+    }
+}
+
